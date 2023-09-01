cce171ea887f8b96b562b03f5d81d48f1a858aa1
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-09-01 12:55:40 +0300

    init

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,20 +1,40 @@
-import orderByProps from '../app';
+import getDataAttack from '../app';
 
-test('testOrderByProps', () => {
-  const obj = {
-    name: 'мечник',
-    health: 10,
-    level: 2,
-    attack: 80,
-    defence: 40,
+test('testGetDataAttack', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
   };
 
-  const result = orderByProps(obj, ['name', 'level']);
-  expect(result).toEqual([
-    { key: 'name', value: 'мечник' },
-    { key: 'level', value: 2 },
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
-    { key: 'health', value: 10 },
-  ]);
+  const result = getDataAttack(character);
+  expect(result).toEqual([{
+    id: 8,
+    name: 'Двойной выстрел',
+    icon: 'http://...',
+    description: 'Двойной выстрел наносит двойной урон',
+  },
+  {
+    id: 9,
+    name: 'Нокаутирующий удар',
+    icon: 'http://...',
+    description: 'Описание недоступно',
+  }]);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,13 @@
-export default function orderByProps(obj, [...keys]) {
-  const includedKeys = [];
-  const sortedKeys = [];
-  for (const property in obj) {
-    if (keys.includes(property)) {
-      includedKeys.push({
-        key: property,
-        value: obj[property],
-      });
-    } else {
-      sortedKeys.push({
-        key: property,
-        value: obj[property],
-      });
-    }
-  }
-  sortedKeys.sort((a, b) => {
-    if (Object.values(a) < Object.values(b)) {
-      return -1;
-    }
-    return 1;
+export default function getDataAttack(obj) {
+  const result = [];
+  const { special } = obj;
+  special.forEach((prop) => {
+    const {
+      id, name, description = 'Описание недоступно', icon,
+    } = prop;
+    result.push({
+      id, name, description, icon,
+    });
   });
-  return [...includedKeys, ...sortedKeys];
+  return result;
 }
